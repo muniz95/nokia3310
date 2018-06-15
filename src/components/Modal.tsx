bdc91de37957473e0a684043a51aed810531d5d1
Rodrigo Muniz <rodrigo.temiski1995@gmail.com>
2018-06-14 22:59:10 -0300

    Modal layout improvements

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,9 +1,14 @@
 import * as React from "react";
+import '../styles/Modal.scss';
 
 const Modal = () => (
   <div className="modal">
     <div className="modal-content">
-      Oe
+      <input type="checkbox" name="ok" id="ok" style={{display: 'none'}} />
+      <label htmlFor="ok">
+        <span>&nbsp;</span>
+        <ins><i>&nbsp;</i></ins>
+      </label>
     </div>
   </div>
 );
